c3fd0011ae6e440c73e2113d2fa804c0ab13ecef
johker1 <me@johker.xyz>
2017-09-27 20:01:13 +0100

    update start.sh

--- setup/start.sh
+++ setup/start.sh
@@ -111,6 +111,7 @@ source setup/zpush.sh
 source setup/management.sh
 source setup/munin.sh
 source setup/spreedme.sh
+source setup/collabora.sh
 
 # Wait for the management daemon to start...
 until nc -z -w 4 127.0.0.1 10222
