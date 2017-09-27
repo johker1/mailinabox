8dd33ef703abcbb576b563722150b475c5464ab7
johker1 <me@johker.xyz>
2017-09-27 16:37:30 +0100

    spreedme integration

--- setup/start.sh
+++ setup/start.sh
@@ -110,6 +110,7 @@ source setup/owncloud.sh
 source setup/zpush.sh
 source setup/management.sh
 source setup/munin.sh
+source setup/spreedme.sh
 
 # Wait for the management daemon to start...
 until nc -z -w 4 127.0.0.1 10222
