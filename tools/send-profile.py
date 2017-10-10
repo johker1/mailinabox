2cb5506a53410973c3015d1614db3f7f58d245b5
johker1 <me@johker.xyz>
2017-10-10 18:43:17 +0100

    send-profile.py added to send working OSX email profile

--- tools/send-profile.py
+++ tools/send-profile.py
@@ -1,16 +1,13 @@
-#!/usr/bin/python3
 
-
-
-import smtplib, sys
+import smtplib, sys, os
 from os.path import basename
 from email.mime.application import MIMEApplication
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
+from shutil import copyfile
 
-
-def send_mail(send_from, send_to, subject, text, files=None,
+def send_mail(send_from, send_to, subject, text, file,
               server="127.0.0.1"):
     assert isinstance(send_to, list)
 
@@ -22,22 +19,37 @@ def send_mail(send_from, send_to, subject, text, files=None,
 
     msg.attach(MIMEText(text))
 
-    for f in files or []:
-        with open(f, "rb") as fil:
-            part = MIMEApplication(
-                fil.read(),
-                Name=basename(f)
-            )
+    with open(file, "rb") as fil:
+        part = MIMEApplication(
+            fil.read(),
+            Name=basename(file)
+        )
         # After the file is closed
-        part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+        part['Content-Disposition'] = 'attachment; filename="%s"' % basename("osx.mobileconfig")
         msg.attach(part)
 
     smtp = smtplib.SMTP(server)
     smtp.sendmail(send_from, send_to, msg.as_string())
     smtp.close()
 
-send_mail("team@cloudfirst.pt",sys.argv[1],
-            "OSX e IOS Profile",
-            "Your Profile for Mail, Calendar and Contacts\n Just double-click the attachment!",
-            files=["/var/lib/mailinabox/mobileconfig.xml"]
-            )
+try:
+        os.remove("/var/lib/mailinabox/mobileconfig.xml.LASTSENT")
+except OSError:
+        pass
+
+with open('/var/lib/mailinabox/mobileconfig.xml') as infile, open('/var/lib/mailinabox/mobileconfig.xml.LASTSENT', 'w') as outfile:
+        for line in infile:
+                line= line.replace('USER_EMAIL', sys.argv[1])
+                outfile.write(line)
+
+del sys.argv[0]
+
+send_mail("team@cloudfirst.pt",
+                [sys.argv[0]],
+                "OSX e IOS Profile",
+                'Para configurar o email, calendário e contactos basta utilizar o perfil em anexo.\n'
+                'Para tal basta salvar o anexo e fazer double-click no ficheiro osx.mobileconf\n'
+                'Preencha com o seu email e password em todos os campos\n\n'
+                ' Obrigado e Bem Vindo!',
+                "/var/lib/mailinabox/mobileconfig.xml.LASTSENT")
+
